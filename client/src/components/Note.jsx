a7c90dfa10167cc343e97eddf51a772346d101a0
itzkerry <mauryakrishna098@gmail.com>
2025-06-30 05:02:31 +0530

    fix note.jsx

--- client/src/components/Note.jsx
+++ client/src/components/Note.jsx
@@ -13,14 +13,14 @@ const ViewNote = () => {
         <div className='flex flex-row gap-7 place-content-between'>
             <input
             type="text"
-            className='p-2 border-2 border-gray-200 rounded-lg w-[70%]'
+            className='p-2 border-2 border-gray-200 rounded-lg w-full font-bold'
             placeholder='Enter Title here'
             value={notes.title} 
             disabled/>
         </div>
-        <div className="">
+        <div className="w-full">
             <textarea
-                className='rounded-lg mt-6 min-w-[500px] p-4 border border-gray-400'
+                className='rounded-lg mt-6 p-4 border w-full border-gray-400'
                 value={notes.content}
                 placeholder='Enter content here'
                 rows={20}
